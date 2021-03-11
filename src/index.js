a264cb2bb762ce04131ac4a01a82f64f44dfd01a
AngelinaZh <angelinka1921@gmail.com>
2021-03-11 14:10:31 +0300

    feat:update with additing information

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
+    let reversed = n.toString().split("").reverse().join("");
+    reversed = parseInt(reversed);
   
+    if (n < 0) {
+      return reversed ;
+    }
+    return reversed;
 }
